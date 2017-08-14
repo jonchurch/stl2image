245ea7ad9399a5ac0686d6d88ba89665de8b834b
jonchurch <me@jonchurch.com>
2017-08-13 21:43:27 -0400

    add s3 client

--- stl2image.js
+++ stl2image.js
@@ -1,5 +1,15 @@
 const stl2image = require('stljs')
 const resolve = require('path').resolve
+const s3 = require('s3')
+
+var client = s3.createClient({
+  s3Options: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY
+    // any other options are passed to new AWS.S3() 
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+  },
+});
 
 // destination on disk to save the png we create
 const IMAGE_DESTINATION = resolve('./file_db/png/')
@@ -15,7 +25,10 @@ module.exports = async (req, res, next) => {
 	const stlFilePath = STL_FILE.path
 	console.log({stlFilePath})
 
-	const imagePath = `${IMAGE_DESTINATION}/${STL_FILE.filename.slice(0, -4)}.png` 
+	const imageKey = `${STL_FILE.filename.slice(0, -4)}`
+
+	const imagePath = `${IMAGE_DESTINATION}/${imageKey}.png`
+	console.log
 
 	console.log({IMAGE_DESTINATION})
 	console.log({imagePath})
@@ -32,6 +45,27 @@ module.exports = async (req, res, next) => {
 				} else {
 					// res.send(povOutput)
 					resolve(povOutput)
+					var params = {
+					localFile: imagePath,
+					
+					s3Params: {
+						Bucket: "farlab-images",
+						Key: imageKey,
+						// other options supported by putObject, except Body and ContentLength. 
+						// See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
+					},
+					};
+					var uploader = client.uploadFile(params);
+					uploader.on('error', function(err) {
+					console.error("unable to upload:", err.stack);
+					});
+					uploader.on('progress', function() {
+					console.log("progress", uploader.progressMd5Amount,
+								uploader.progressAmount, uploader.progressTotal);
+					});
+					uploader.on('end', function() {
+					console.log("done uploading");
+					});
 
 					
 				}
